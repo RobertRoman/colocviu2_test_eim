87226201aac0d5f110628dfe70cef7c5b025e05b
robert <robertroman13@yahoo.com>
2020-05-15 20:21:37 +0300

    initial commit

---
+++ app/src/main/java/ro/pub/cs/systems/eim/colocviu2test/Colocviu2TestMainActivity.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.colocviu2test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Colocviu2TestMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
